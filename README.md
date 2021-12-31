50e50fe5fefb2a691448ef804fb87f20a4849b75
Yashwanth Parameswaran (MS/EMT3-XC) <yap1cob@bosch.com>
2021-12-31 21:09:21 +0530

    Batch script to login Cisco VPN automatically

---
+++ README.md
@@ -0,0 +1,1 @@
+# CISCO_VPN_AutoConnect
---
+++ VPN_AutoLogin.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\vpncli.exe" -s < C:\Users\YAP1COB\vpn.login_info
+"C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\vpnui.exe"
