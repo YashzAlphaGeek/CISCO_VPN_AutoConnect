e76fa19124ffe53df60a2e80762c8c04f800728e
Yashwanth <yashwa23@gmail.com>
2022-01-02 18:55:20 +0530

    Killing of the existing VPN connection

--- VPN_AutoLogin.bat
+++ VPN_AutoLogin.bat
@@ -1,3 +1,4 @@
 @echo off
-"C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\vpncli.exe" -s < C:\Users\Yashwanth\vpn.login_info
+tasklist | find /i "vpnui.exe" && taskkill /im vpnui.exe /F || echo process "vpnui.exe" not running.
+"C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\vpncli.exe" -s < C:\Users\YAP1COB\vpn.login_info
 "C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\vpnui.exe"
