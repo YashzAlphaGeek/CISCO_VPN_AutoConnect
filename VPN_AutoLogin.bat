93c618bcd5bc730b96eed9c1f56c4fc7ec362eff
Yashwanth <yashwa23@gmail.com>
2022-01-02 18:59:57 +0530

    Modified batch script

--- VPN_AutoLogin.bat
+++ VPN_AutoLogin.bat
@@ -1,4 +1,4 @@
 @echo off
 tasklist | find /i "vpnui.exe" && taskkill /im vpnui.exe /F || echo process "vpnui.exe" not running.
-"C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\vpncli.exe" -s < C:\Users\YAP1COB\vpn.login_info
+"C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\vpncli.exe" -s < C:\Users\Yashwanth\vpn.login_info
 "C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\vpnui.exe"
